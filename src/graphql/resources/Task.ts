78d4d9552cbec9d760a64fb79490ae3a0d956394
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-06-10 00:49:11 -0400

    chores: rename indexes with .ts extension

--- src/graphql/resources/Task.ts
+++ src/graphql/resources/Task.ts
@@ -1,12 +1,5 @@
 import { gql } from '@apollo/client';
 
-export interface Task {
-  id: number;
-  title: string;
-  decsription: string | null;
-  completed: boolean;
-}
-
 export const GetAllTasksQuery = gql`
   query GetAllTasks {
     getAllTasks {
---
+++ src/interfaces/Task.ts
@@ -0,0 +1,6 @@
+export interface Task {
+  id: number;
+  title: string;
+  decsription: string | null;
+  completed: boolean;
+}
