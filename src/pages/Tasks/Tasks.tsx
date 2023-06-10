c16b890ce4d827ef3e5f3001520134bf53c43369
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-06-10 00:28:06 -0400

    feature: add navbar

--- src/pages/Tasks/Tasks.tsx
+++ src/pages/Tasks/Tasks.tsx
@@ -1,12 +1,24 @@
 import { useQuery } from '@apollo/client';
 import { GetAllTasksQuery } from '../../graphql/resources/Task';
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
+import { AccountCircle } from '@mui/icons-material';
 
 export function TasksPage() {
   const { loading, error, data } = useQuery(GetAllTasksQuery);
 
   return (
-    <div className="TasksPage">
-      <h1>Tasks</h1>
-    </div>
+    <Box>
+      <AppBar position="static">
+        <Toolbar style={{ justifyContent: 'space-between' }}>
+          <Typography component={'h1'} variant="h4">
+            Simple Task
+          </Typography>
+
+          <IconButton>
+            <AccountCircle fontSize="large" color="inherit" />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
