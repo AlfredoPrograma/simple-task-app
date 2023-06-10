6bb37997a88b737579d9796f52a6957ebcaa2252
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-06-09 23:30:59 -0400

    feature: initialize graphql client

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GRAPHQL_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
